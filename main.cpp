5f2af7eefbd2a44442e60cd1ac7233d1504d5668
Théo <48034060+theo-boi@users.noreply.github.com>
2022-04-19 17:11:28 +0200

    Correction de: - contraintes de shifts consécutifs minimum - durée de travail maximum Reste : - durée de travail minimum (implémenter Duree_Minimum_Combinaison_Shift_Possible)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <chrono>
 #include <vector>
+#include <map>
 #include "Instance.hpp"
 #include "Solution.hpp"
 
@@ -17,7 +18,7 @@ int Resolution(Instance* instance);
 
 Solution * GenerationSolutionRealisable(Instance * instance);
 
-bool is_Peut_Reprendre_Travaille(Instance* instance, int personne, int jour, int Nbre_WE_Travaille_Personne);
+bool is_Peut_Reprendre_Travaille(Instance* instance, int personne, int jour, int Nbre_WE_Travaille_Personne, int duree_totale_shift_personne, int duree_shift_max);
 
 
 int main(int argc, const char* argv[])
@@ -140,14 +141,15 @@ int Resolution(Instance* instance)
 
     std::cout << uneSolution->Verification_Solution(instance);
 
-    //i_val_Retour_Fct_obj = uneSolution->i_valeur_fonction_objectif;
-    //delete uneSolution;
-    //return i_val_Retour_Fct_obj;
-    return 0;
+    i_val_Retour_Fct_obj = uneSolution->i_valeur_fonction_objectif;
+    delete uneSolution;
+    return i_val_Retour_Fct_obj;
 }
 
 
 Solution * GenerationSolutionRealisable(Instance * instance) {
+    // Initialisation
+
     Solution* S = new Solution();
 
     vector<int> v_Nbre_Shift_Consecutif(instance->get_Nombre_Personne());
@@ -156,16 +158,34 @@ Solution * GenerationSolutionRealisable(Instance * instance) {
     vector<int> v_Duree_Totale_Shift_Personne(instance->get_Nombre_Personne());
     vector<vector<int>> v_v_Nbre_Chaque_Shift_Pers(instance->get_Nombre_Personne());
 
+    //map<int, int> m_v_Shift_trie;
+
+    int duree_shift_min = instance->get_Shift_Duree(1);
+    int duree_shift_max = instance->get_Shift_Duree(1);
+    for (int shift = 0; shift < instance->get_Nombre_Shift(); shift++)
+    {
+        int duree_shift = instance->get_Shift_Duree(shift);
+        if (duree_shift_min > duree_shift)
+        {
+            duree_shift_min = duree_shift;
+        }
+        if (duree_shift_max < duree_shift)
+        {
+            duree_shift_max = duree_shift;
+        }
+    }
+
+
     //------Cas du premier jour------
+
     S->v_v_IdShift_Par_Personne_et_Jour = vector<vector<int>>(instance->get_Nombre_Personne());
     for (int personne = 0; personne < instance->get_Nombre_Personne(); personne++)
     {
-        cout << "Boucle personne : " << personne << endl;
         S->v_v_IdShift_Par_Personne_et_Jour[personne] = vector<int>(instance->get_Nombre_Jour(), -1);
         v_v_Nbre_Chaque_Shift_Pers[personne] = vector<int>(instance->get_Nombre_Shift(), 0);
         int shift = -1;
 
-        if ((instance->is_Available_Personne_Jour(personne, 0)) && (is_Peut_Reprendre_Travaille(instance, personne, 0, 0)))
+        if ( instance->is_Available_Personne_Jour(personne, 0) && is_Peut_Reprendre_Travaille(instance, personne, 0, 0, v_Duree_Totale_Shift_Personne.at(0), duree_shift_max) )
         {
             int meilleur_candidat = -1;
             int plus_long_Duree_Shift = 0;
@@ -210,41 +230,47 @@ Solution * GenerationSolutionRealisable(Instance * instance) {
         {
             int shift = -1; //Initialisation du shift pour une personne et pour un jour
             
-            if (v_Duree_Totale_Shift_Personne.at(personne) < instance->get_Personne_Duree_total_Max(personne))
+            // Si la durée totale des shifts de la personne avec le plus petit nouveau shift possible ne dépasse pas la durée maximale
+            if (v_Duree_Totale_Shift_Personne.at(personne) + duree_shift_min <= instance->get_Personne_Duree_total_Max(personne))
             {
+                // Si la personne ne travaille pas trop de fois de suite
                 if (v_Nbre_Shift_Consecutif.at(personne) < instance->get_Personne_Nbre_Shift_Consecutif_Max(personne))
                 {
+                    // Si la personne peut travailler ce jour de weekend
                     if (((v_Nbre_WE_Travaille.at(personne) < instance->get_Personne_Nbre_WE_Max(personne)) && (jour % 7 >= 5)) || (jour % 7 < 5))
                     {
+                        // Si la personne n'est pas en congé ce jour là
                         if (instance->is_Available_Personne_Jour(personne, jour))
                         {
-                            if ( ( (v_Nbre_Jour_OFF_Consecutif.at(personne) == 0) ||v_Nbre_Jour_OFF_Consecutif.at(personne) >= instance->get_Personne_Jour_OFF_Consecutif_Min(personne) )
-                                &&
-                                 (is_Peut_Reprendre_Travaille(instance, personne, jour, v_Nbre_WE_Travaille.at(personne))) )
-                                //v_Nbre_Jour_OFF_Consecutif[personne] == 0 :situation dans laquelle la personne travaille depuis au moins le jour d'avant
-                                    // 0 < v_Nbre_Jour_OFF_Consecutif[personne] < get_Personne_Nbre_Jour_OFF_Consecutif_Min(personne)  : situation dans laquelle la personne accumule ses repos consécutifs
-                                    // v_Nbre_Jour_OFF_Consecutif[personne] >= get_Personne_Nbre_Jour_OFF_Consecutif_Min(personne) : situation dans laquelle la personne peut reprendre son travail
-                                // is_Peut_Reprendre_Travaille(personne, jour, v_Nbre_WE_Travaille[personne]) : on vérifie que la personne pourra travailler au moins le nombre consécutif minimum de quart d'heure de travail.
-                            { //choix d'un shift
+                            if (    ( v_Nbre_Jour_OFF_Consecutif.at(personne) == 0 ) // Si la personne travaille depuis au moins le jour d'avant
+                                 || (    v_Nbre_Jour_OFF_Consecutif.at(personne) >= instance->get_Personne_Jour_OFF_Consecutif_Min(personne) // Si la personne a eu assez de repos et peut reprendre son travail
+                                      && is_Peut_Reprendre_Travaille(instance, personne, jour, v_Nbre_WE_Travaille.at(personne), v_Duree_Totale_Shift_Personne.at(personne), duree_shift_max) ) ) // Et que la personne pourra travailler au moins le nombre consécutif minimum de quart d'heure de travail.
+                            {
+                                // Choix d'un shift
                                 bool is_chosen_shift = false;
                                 for (int candidat = 0; (candidat < instance->get_Nombre_Shift()) && (!is_chosen_shift); candidat++) //les id shift correspondent à candidat
                                 {
-                                    if (S->v_v_IdShift_Par_Personne_et_Jour.at(personne).at(jour-1) == -1) // Si le jour précédent était un jour repos
+                                    // Si la durée totale des shifts de la personne avec le candidat ne dépasse pas la durée maximale
+                                    if (v_Duree_Totale_Shift_Personne.at(personne) + instance->get_Shift_Duree(candidat) <= instance->get_Personne_Duree_total_Max(personne))
                                     {
-                                        if (v_v_Nbre_Chaque_Shift_Pers.at(personne).at(candidat) < instance->get_Personne_Shift_Nbre_Max(personne, candidat))
+                                        if (S->v_v_IdShift_Par_Personne_et_Jour.at(personne).at(jour-1) == -1) // Si le jour précédent était un jour repos
                                         {
-                                            shift = candidat;
-                                            is_chosen_shift = true;
+                                            if (v_v_Nbre_Chaque_Shift_Pers.at(personne).at(candidat) < instance->get_Personne_Shift_Nbre_Max(personne, candidat))
+                                            {
+                                                shift = candidat;
+                                                is_chosen_shift = true;
+                                            }
+                                        }
+                                        else if ( instance->is_possible_Shift_Succede(S->v_v_IdShift_Par_Personne_et_Jour.at(personne).at(jour-1), candidat) ) // Sinon, on vérifie que le candidat peut succéder au jour de travail précédent
+                                        {
+                                            if (v_v_Nbre_Chaque_Shift_Pers.at(personne).at(candidat) < instance->get_Personne_Shift_Nbre_Max(personne, candidat))
+                                            {
+                                                shift = candidat;
+                                                is_chosen_shift = true;
+                                            }
                                         }
                                     }
-                                    else if ( instance->is_possible_Shift_Succede(S->v_v_IdShift_Par_Personne_et_Jour.at(personne).at(jour-1), candidat) ) // Sinon, on vérifie que le candidat peut succéder au jour de travail précédent
-                                    {
-                                        if (v_v_Nbre_Chaque_Shift_Pers.at(personne).at(candidat) < instance->get_Personne_Shift_Nbre_Max(personne, candidat))
-                                        {
-                                            shift = candidat;
-                                            is_chosen_shift = true;
-                                        }
-                                    }
+                                    
                                 }
                             }
                         }
@@ -279,14 +305,56 @@ Solution * GenerationSolutionRealisable(Instance * instance) {
 }
 
 
-bool is_Peut_Reprendre_Travaille(Instance * instance, int personne, int jour, int Nbre_WE_Travaille_Personne)
+bool is_Peut_Reprendre_Travaille(Instance * instance, int personne, int jour, int Nbre_WE_Travaille_Personne, int duree_totale_shift_personne, int duree_shift_max)
 {
-    for (int i = jour; (i < jour + instance->get_Personne_Nbre_Shift_Consecutif_Min(personne)) && (i < instance->get_Nombre_Jour()); i++)  //On vérfiie que la personne est disponible sur la nombre de jour minimum consécutif suivant (contraintre shift consécutif minimum) )
+    // Pour chaque jour suivants sur lesquels la personne devra travailler ensuite
+    for (int i = jour; (i < jour + instance->get_Personne_Nbre_Shift_Consecutif_Min(personne)) && (i < instance->get_Nombre_Jour()); i++)  //On vérifie que la personne est disponible sur le nombre de jours consécutifs suivants minimum
     {
-        if ((!instance->is_Available_Personne_Jour(personne, i)) || (Nbre_WE_Travaille_Personne >= instance->get_Personne_Nbre_WE_Max(personne) && i % 7 >= 5))
+        if (    (!instance->is_Available_Personne_Jour(personne, i)) // Si la personnne est en congé ce jour là
+             || (    Nbre_WE_Travaille_Personne >= instance->get_Personne_Nbre_WE_Max(personne) // Ou qu'elle s'apprête à travailler trop de weekends
+                  && i % 7 >= 5 ) // Et que c'est le weekend
+             || (duree_totale_shift_personne + duree_shift_max * instance->get_Personne_Nbre_Shift_Consecutif_Min(personne) > instance->get_Personne_Duree_total_Max(personne)) ) // Ou la personne ne dépassera pas sa durée maximale de travail
         {
             return false;
         }
     }
     return true;
 }
+
+
+int Duree_Minimum_Combinaison_Shift_Possible(...) {
+    
+}
+
+/*
+meilleur_combinaison_shift-consecutif_duree_minimum(tableau_Id-Shift_trié, personne):
+    int combinaison_shift [nb_shift_consecutif_min]
+    bool iniisalise = false
+    int shift =0
+    Tant que (! initialise)
+        si get_Personne_Shift_Nbre_Max(personne,shift) > 0
+            comobinaison_shift[0]= shift
+            initilise = true
+        fin Si
+        shift++
+    fin Tant QUe
+    int dureeTotal =0
+    for (i = 1; i <nb_shift_consecutif_min; i++)
+        bool is_chosen = false
+        shift = 0
+        tant que ( !is_chosen && shift < instance->get_Nombre_Shift())
+            si get_Personne_Shift_Nbre_Max(personne,shift) > 0 &&  is_possible_Shift_Succede(combinaison_shift[i-1],shift)
+                combinsaison_shift[i] = shift
+                is_chosen = true
+            FinSi
+            shift++
+        fin Tantque
+        si(shift >= instance->get_nombre_Shift())
+            break pour
+            Afficher("Erreur combinaison minimum impossible")
+        fin si
+        dureeeTotale += instance->get_Shift_Duree(combinaison_shift[i])
+    fin pour
+    return dureeTotale
+
+*/
